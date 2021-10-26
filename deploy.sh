96e72c507f8c1f6db7676cc5cd9d91e09e6d1c55
willian <willianr.demelo@gmail.com>
2021-10-26 19:31:42 +0000

    Corrigindo para rodar no jenkins

--- deploy.sh
+++ deploy.sh
@@ -178,4 +178,4 @@ cat <<EOF > 2-provisionar-k8s-master-auto-shell.yml
       shell: kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=\$(kubectl version | base64 | tr -d '\n')"
 EOF
 
-ansible-playbook -i hosts 2-provisionar-k8s-master-auto-shell.yml -u ubuntu --private-key ~/.ssh/id_rsa
+ansible-playbook -i hosts 2-provisionar-k8s-master-auto-shell.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
---
+++ teste.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd 2-ansible/01-k8s-install-masters_e_workers/
+
+ansible-playbook -i hosts teste.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
